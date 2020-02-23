c064a5e3f5e13905fc08519b960cd0b6c4706edd
AdvisingAI <admin@advisingai.com>
2020-02-23 18:31:07 -0500

    fixing things

---
+++ src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { HomeModule } from './home/home.module';
+import { LoginComponent } from './login/login.component';
+
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8F_ZRgFr4EPocts9QOvpmfKMnQ2bDAjQ",
+  authDomain: "advisingai-landing.firebaseapp.com",
+  databaseURL: "https://advisingai-landing.firebaseio.com",
+  projectId: "advisingai-landing",
+  storageBucket: "advisingai-landing.appspot.com",
+  messagingSenderId: "523410219746",
+  appId: "1:523410219746:web:7b5a86d9123feb5d7a74bb",
+  measurementId: "G-6V9ZZH7Y12"
+};
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    LandingComponent,
+    ProfileComponent,
+    NavbarComponent,
+    FooterComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot(),
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+    HomeModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
