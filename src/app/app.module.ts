137f8af691f0caa7565c1cc1c7e3bd1a1ee794b5
AdvisingAI <admin@advisingai.com>
2020-02-23 18:06:51 -0500

    adding a contact form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,22 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HomeModule } from './home/home.module';
 import { LoginComponent } from './login/login.component';
 
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8F_ZRgFr4EPocts9QOvpmfKMnQ2bDAjQ",
+  authDomain: "advisingai-landing.firebaseapp.com",
+  databaseURL: "https://advisingai-landing.firebaseio.com",
+  projectId: "advisingai-landing",
+  storageBucket: "advisingai-landing.appspot.com",
+  messagingSenderId: "523410219746",
+  appId: "1:523410219746:web:7b5a86d9123feb5d7a74bb",
+  measurementId: "G-6V9ZZH7Y12"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,9 +48,11 @@ import { LoginComponent } from './login/login.component';
     FormsModule,
     RouterModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
